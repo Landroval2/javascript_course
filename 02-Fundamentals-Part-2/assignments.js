8094505a310857a415d8d32f2af9ec198407a8d4
Nahuel Garcia <nahuelgarcia226@gmail.com>
2022-08-07 17:32:07 -0300

    ADD - Functions assignments

--- 02-Fundamentals-Part-2/assignments.js
+++ 02-Fundamentals-Part-2/assignments.js
@@ -13,3 +13,34 @@ const usa = describeCountry("United States", 300, "Washington D. C.");
 console.log(argentina);
 console.log(finland);
 console.log(usa);
+
+// LECTURE: Function Declarations vs. Expressions
+
+const totalPopulation = 7900;
+function percentageOfWorld1(population) {
+  return (population / totalPopulation) * 100;
+}
+
+const populationChina = 1441;
+const populationArgentina = 45;
+const populationFinland = 6;
+
+console.log(percentageOfWorld1(populationChina));
+console.log(percentageOfWorld1(populationArgentina));
+console.log(percentageOfWorld1(populationFinland));
+
+const percentageOfWorld2 = function (population) {
+  return (population / totalPopulation) * 100;
+};
+
+console.log(percentageOfWorld2(populationChina));
+console.log(percentageOfWorld2(populationArgentina));
+console.log(percentageOfWorld2(populationFinland));
+
+// Lecture Arrow Functions:
+
+const percentageOfWorld3 = (population) => (population / totalPopulation) * 100;
+
+console.log(percentageOfWorld2(populationChina));
+console.log(percentageOfWorld2(populationArgentina));
+console.log(percentageOfWorld2(populationFinland));
--- 02-Fundamentals-Part-2/script.js
+++ 02-Fundamentals-Part-2/script.js
@@ -24,3 +24,43 @@
 // }
 // const appleJuice = fruitProcessor(5, 0);
 // console.log(appleJuice);
+
+// Function declaration
+// function calcAge1(birthYear) {
+//   return 2037 - birthYear;
+// }
+
+// const age1 = calcAge1(1990);
+// console.log(age1);
+
+// // Function expression
+// const calcAge2 = function (birthYear) {
+//   return 2037 - birthYear;
+// };
+
+// const age2 = calcAge2(1990);
+// console.log(age2);
+
+// const age3 = calcAge2(1995);
+// console.log(age3);
+
+// Arrow function
+
+// const calcAge3 = (birthYear) => 2037 - birthYear;
+// console.log(calcAge3(2001));
+
+// const yearsUntilRetirement = (birthYear, firstName) => {
+//   const age = 2037 - birthYear;
+//   const retirement = 65 - age;
+//   return `${firstName} retires in ${retirement} years`;
+// };
+
+// console.log(yearsUntilRetirement(1991, "Jonas"));
+// console.log(yearsUntilRetirement(1985, "Bob"));
+
+function fruitProcessor(apples, oranges) {
+  console.log(apples, oranges);
+  const juice = ` Juice with ${apples} apples
+    and ${oranges} oranges`;
+  return juice;
+}
